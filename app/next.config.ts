1d546c4d2289cd223148553678146d923adb1d21
Olga Slovinska <olga.slovinska@vaimo.com>
2026-03-12 14:04:26 +0200

    Task(advanced): implement Cache Components with cacheLife and cacheTag

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    cacheComponents: true,
+    products: {
+        stale: 300, // 5 minutes
+        revalidate: 900, // 15 minutes
+        expire: 3600, // 1 hour
+    }
 };
 
 export default nextConfig;
