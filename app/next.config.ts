8607d5089d19d40a02bd438125621600380d4f69
Olga Slovinska <olga.slovinska@vaimo.com>
2026-03-12 14:05:12 +0200

    Task(advanced): rollback

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    cacheComponents: true,
-    products: {
-        stale: 300, // 5 minutes
-        revalidate: 900, // 15 minutes
-        expire: 3600, // 1 hour
-    }
+  /* config options here */
 };
 
 export default nextConfig;
